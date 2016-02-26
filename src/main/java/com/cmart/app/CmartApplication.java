33b2ff90c765dc8a3d8e751323c6911f5057edef
goestbag <dedddy@gmail.com>
2016-02-26 16:08:52 +0700

    template project

---
+++ src/main/java/com/cmart/app/CmartApplication.java
@@ -0,0 +1,12 @@
+package com.cmart.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmartApplication.class, args);
+	}
+}
